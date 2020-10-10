7ec8b261eb97c10d0d614fd437984734d682a5b8
Flev <flavio.gavarini@issgreppi.it>
2020-10-10 18:23:47 +0200

    Modifica file Flavio

---
+++ Flavio/EsercitazionePerLunedì/Flavio.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EsercitazionePerLunedì;
+
+namespace EsercitazionePerLunedì
+{
+    public class Flavio
+    {
+        public string Nome{get; set;}
+        public string Cognome { get; set; }
+        public static void Flev()
+        {
+            Console.WriteLine("flavio Gavarini");
+        }
+        public static void Persona(Flavio flavio)
+        {
+            
+            Console.WriteLine(flavio.Nome +" "+flavio.Cognome);
+        }           
+    }
+}
---
+++ Flavio/EsercitazionePerLunedì/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace EsercitazionePerLunedì
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+            Flavio flavio = new Flavio();
+        }
+    }
+}
